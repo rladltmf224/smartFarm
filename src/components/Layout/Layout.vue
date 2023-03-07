1cb353de61b3a628a5b87862f07f0277716c18eb
rladltmf224 <yskim1twim@gmail.com>
2023-03-07 17:46:05 +0900

    style:theme color green으로 변경

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="pa-6">
     <Sidebar />
-    <v-main class="content">
+    <v-main class="content background-color ">
       <router-view></router-view>
     </v-main>
   </v-app>
@@ -15,7 +15,16 @@ import Sidebar from "@/components/Sidebar/Sidebar.vue";
     Sidebar,
   },
 })
-export default class Layout extends Vue {}
+export default class Layout extends Vue { }
 </script>
 
 <style src="./Layout.scss" lang="scss"></style>
+
+
+
+<style lang="css">
+.background-color {
+  background: #F1F8E9
+}
+</style>
+
