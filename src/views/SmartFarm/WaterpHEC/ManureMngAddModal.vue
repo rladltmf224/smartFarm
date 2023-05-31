02a7e1dd63acb5c7e088426ecab83fa8206e800a
yeeeerin <jyr950401@gmail.com>
2023-05-31 16:13:18 +0900

    feat : 양액 관리 - 화면 구성

--- src/views/SmartFarm/WaterpHEC/ManureMngAddModal.vue
+++ src/views/SmartFarm/WaterpHEC/ManureMngAddModal.vue
@@ -14,13 +14,17 @@
             disable-pagination
             hide-default-footer
             show-select
+            loading-text="서버에 요청중...."
+            no-data-text="데이터가 없습니다."
+            class="elevation-1"
+            fixed-header
           >
           </v-data-table>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="success">사용완료</v-btn>
-          <v-btn color="error" @click="manureDialog = false">닫기</v-btn>
+          <v-btn color="error" @click="close">닫기</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -43,6 +47,9 @@ export default class ManureMngAddModal extends Vue {
 
   created() {
     this.manureAdd_headers = Object.assign([], cfg.header.manureAdd_headers);
+  }
+  close() {
+    this.$emit("closeModal", false);
   }
 }
 </script>
